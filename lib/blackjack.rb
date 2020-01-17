cecb9a19f4b936ff2c7f7af2c9d3098717027d3a
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-17 20:27:51 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,10 +54,10 @@ end
 def runner
   welcome
   card_total = initial_round
-  hit?(card_total)
+  while card_total < 21
+  card_total = hit?(card_total)
   display_card_total(card_total)
-  prompt_user
-  if card_total > 21
-  end_game
+  end  
+  end_game(card_total)
 end
-end  
+ 
